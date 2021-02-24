f2366bdec5d9ccebcaf82d9f6e3dd810b63436ed
Yukidama <narururu19@gmail.com>
2021-02-24 11:31:27 +0900

    horner's method

--- algorithms/horner.cpp
+++ algorithms/horner.cpp
@@ -1,10 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define rep(i, n) for (int i = 0; i < n; ++i)
-
 // 入力は n a_n a_n-1...a_0 x(n次多項式 x^nの係数...x^0の係数 xの値)
 // 入力形式はint
-
 int horner(int A[],int n,int x);
 int main(){
     int n;
