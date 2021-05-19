31f64c9f45d17a9ea8291a7b2d92dd1d0c459a76
Yukidama <narururu19@gmail.com>
2021-05-19 17:40:43 +0900

    修正、conv2dパラメータ計算

--- algorithms/others/calc_params_conv2d.cpp
+++ algorithms/others/calc_params_conv2d.cpp
@@ -6,19 +6,20 @@ using ll = long long;
 
 #define DEBUG_ON
 ll outsize(ll in,ll st,ll pad,ll ks){
-    return (in+2*pad-ks)/st;
+    return (in+2*pad-ks)/st+1;
 }
 
 void decopri(string txt){
-    rep(i,50){
+    ll st=100;
+    rep(i,st){
         cout<<"☆";
     }
     cout << "" << endl;
-    rep(i,50-(txt.length())/2){
+    rep(i,(st-txt.length())/2){
         cout << " ";
     }
     cout<<txt<<endl;
-    rep(i,50){
+    rep(i,st){
         cout<<"☆";
     }
     cout<<""<<endl;
@@ -33,6 +34,15 @@ int main(){
     ll in;
     cin>>in;
     ll out;
+    string txt="欲しいサイズ ";
+    ostringstream oss;
+    oss<<o_p;
+    txt+=oss.str();
+    txt+=",　入力サイズ ";
+    ostringstream oss2;
+    oss2 << in;
+    txt += oss2.str();
+    decopri(txt);
     rep(i,10){
         if(i==0)continue;
         rep(j,10){
@@ -40,7 +50,7 @@ int main(){
                 out=outsize(in,i,j,k);
                 if(out>1&&out<=28){
                     if(out==o_p)
-                    cout<<out<<":kernel size "<<k<<",stride "<<i<<",padding "<<j<<endl;
+                    cout<<"kernel size "<<k<<",stride "<<i<<",padding "<<j<<endl;
                 }
             }
         }
